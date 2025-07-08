e787a09b8e7b09a893df52a2ed6773d398e8e3b4
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-07-08 11:40:34 +0900

    사칙연산까지 개발

--- src/main/java/org/example/Lv1/CalculatorFirst.java
+++ src/main/java/org/example/Lv1/CalculatorFirst.java
@@ -1,7 +1,33 @@
 package org.example.Lv1;
 
+import java.util.Scanner;
+
 public class CalculatorFirst {
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요:");
+        int num1= sc.nextInt();
+
+        System.out.println("두 번째 숫자를 입력하세요:");
+        int num2= sc.nextInt();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        String charAt = sc.next();
+        int num3 = 0;
+
+        if (charAt.equals("+")) {
+            num3 = num1 + num2;
+        } else if (charAt.equals("-")) {
+            num3 = num1 - num2;
+        } else if (charAt.equals("*")) {
+            num3 = num1 * num2;
+        }  else if (charAt.equals("/")) {
+            num3 = num1 / num2;
+        }  else {
+            System.out.println("정확한 사칙연산 기호를 입력하세요.");
+        }
+        System.out.println("결과:"+ num3);
+
     }
 }
