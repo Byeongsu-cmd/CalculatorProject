315230582341bcdc9807e999b8fda2eec2795371
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-07-09 13:49:50 +0900

    CalculatorSecond.java 작업 저장

--- src/main/java/org/example/Lv2/CalculatorSecond.java
+++ src/main/java/org/example/Lv2/CalculatorSecond.java
@@ -1,4 +1,9 @@
 package org.example.Lv2;
 
-public class CalculatorSecond {
+import org.example.Lv1.CalculatorFirst;
+
+import java.util.Scanner;
+
+public class CalculatorSecond extends CalculatorFirst{
+
 }
