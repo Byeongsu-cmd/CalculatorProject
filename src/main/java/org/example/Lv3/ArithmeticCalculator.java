83d145e784115c2456877bded57b3ac1d8387e1e
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-07-14 15:54:48 +0900

    기본 틀 저장

--- src/main/java/org/example/Lv3/ArithmeticCalculator.java
+++ src/main/java/org/example/Lv3/ArithmeticCalculator.java
@@ -1,42 +1,50 @@
 package org.example.Lv3;
-
-import java.util.InputMismatchException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class ArithmeticCalculator {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        Calculate<Double> resultList = new Calculate<>();
 
         while (true) {
             try {
-                System.out.print("첫번째 숫자를 입력하세요: ");
-                double num1 = sc.nextDouble();
+                System.out.print("첫 번째 숫자를 입력하세요:");
+                int num1 = sc.nextInt();
 
-                System.out.print("연산 기호를 입력하세요(+,-,*,/): ");
-                String symbol = sc.next();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                String cal = sc.next();
 
-                System.out.print("두번째 숫자를 입력하세요: ");
-                double num2 = sc.nextDouble();
+                System.out.print("두 번째 숫자를 입력하세요:");
+                int num2 = sc.nextInt();
 
-                OperatorType op = OperatorType.newSymbol(symbol);
-                double result = op.apply(num1,num2);
-
-                resultList.addResult(result);
-                System.out.println("결과:"+result);
-
-                System.out.println("저장된 값"+resultList.getResultList());
-
-            } catch (InputMismatchException e) {
-                System.out.println("숫자를 입력하세요.");
+                Integer result = cal1.calculate(num1, num2, cal);
+                if (result != null) {
+                    System.out.println("결과: " + result);
+                    System.out.println("지금까지 저장된 결과들: " + cal1.getResults());
+                }
+                System.out.println("저장된 값을 모두 초기화하고 싶다면 clear를 입력하세요.");
+                System.out.println("1번째 저장된 값을 지우고 싶다면 yes를 입력하세요.");
+                String clear = sc.next();
+                if (clear.equalsIgnoreCase("clear")) {
+                    cal1.setResult(new ArrayList<>());
+                    System.out.println("초기화 완료 되었습니다.");
+                } else if (clear.equalsIgnoreCase("yes")) {
+                    cal1.removeFirst();
+                    System.out.println("1번째 값을 지웠습니다.");
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("숫자가 아닌 값을 입력하셨습니다. 다시 시도해주세요.");
                 sc.nextLine();
                 continue;
             } catch (Exception e) {
                 System.out.println("오류 발생: " + e.getMessage());
             }
-                System.out.println("계속하시겠습니까?(exit 입력 시 종료)");
-                String answer = sc.next();
-                if (answer.equalsIgnoreCase("exit")) break;
+            System.out.println("계속하시겠습니까? (exit 입력 시 종료)");
+            String next = sc.next();
+            if (next.equalsIgnoreCase("exit")) break;
         }
     }
 }
--- src/main/java/org/example/Lv3/OperatorType.java
+++ src/main/java/org/example/Lv3/OperatorType.java
@@ -2,40 +2,26 @@ package org.example.Lv3;
 
 public enum OperatorType {
     add("+"){
-        public double apply(double num1, double num2) {
-            return num1 + num2;
+        public int apply(int num1, int num2){
+            return num1+num2;
         }
     },
     sub("-"){
-        public double apply(double num1, double num2) {
-            return num1 - num2;
+        public int apply(int num1, int num2){
+            return num1-num2;
         }
     },
     mul("*"){
-        public double apply(double num1, double num2) {
-            return num1 * num2;
+        public int apply(int num1, int num2){
+            return num1*num2;
         }
     },
     div("/"){
-        public double apply(double num1, double num2) {
-            if (num2 == 0) {
-                throw new ArithmeticException("0으로 나눌 수 없습니다!");
-            } return num1 / num2;
-        }
-    };
-
-    private final String symbol;
-    OperatorType(String symbol) {
-        this.symbol = symbol;
-    }
-    public abstract double apply(double num1, double num2);
-
-    public static OperatorType newSymbol(String symbol) {
-        for (OperatorType op : OperatorType.values()) {
-            if (op.symbol.equals(symbol)) {
-                return op;
+        public int apply(int num1, int num2){
+            if(num2==0){
+                throw new ArithmeticException("0으로 나눌 수 없습니다.");
             }
+            return num1/num2;
         }
-            throw new IllegalArgumentException("연산자를 다시 입력해주세요.");
     }
 }
